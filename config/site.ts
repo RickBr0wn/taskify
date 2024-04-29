a5407c54272c9b113da091e8b72de8a7e7e4332c
Rick Brown <ricky.brown.00@gmail.com>
2024-04-29 16:31:45 +0100

    adds site metadata and replaces favicon.ico

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfigData = {
+  name: 'Taskify',
+  description:
+    'Taskify simplifies team collaboration and task organization, providing a user-friendly platform for managing projects of any size.',
+}
+
+// Path: config/site.ts
+// Created at: 15:44:06 - 29/04/2024
+// Language: Typescript
+// Framework: React/Next.js
